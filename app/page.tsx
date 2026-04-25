6048262986ad8f9106798e2329dca432a8085ef5
khasanov-ibroxim <xibroxim11@gmail.com>
2026-04-26 00:34:17 +0500

    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import { redirect } from 'next/navigation';
 import { i18n } from '@/i18n-config';
 
 export default function RootPage() {
-    redirect(`/${i18n.defaultLocale}`);
+    return (
+        <html>
+        <head>
+            <meta httpEquiv="refresh" content={`0;url=/${i18n.defaultLocale}/`} />
+        </head>
+        <body />
+        </html>
+    );
 }
